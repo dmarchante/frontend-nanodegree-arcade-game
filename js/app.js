86ffa823e80993c69b4888b1815434b450a33481
David Marchante <cdmarchante@gmail.com>
2017-07-21 16:59:17 -0400

    begin game code

--- js/app.js
+++ js/app.js
@@ -1,19 +1,31 @@
+//Set location
+var Character = function(x, y) {
+    this.x = x;
+    this.y = y;
+};
+//function to move Cahracter
+Character.prototype.move = function() {
+    this.x++;
+    this.y++;
+};
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    Character.call(this, x, y);
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
-
+Enemy.prototype = Object.create(Character.prototype);
+Character.protoype.constructor = Character;
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.move = (this.move * dt);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,8 +36,21 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
+var Player = function(x, y, move) {
+    // Variables applied to instances
+    Character(x, y, move);
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+};
+// Update the players's position
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // Multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.move = (this.move * dt);
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
